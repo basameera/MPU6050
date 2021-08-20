058786e962864ae6e50368048e1e52d5044578f6
basameera <basameera@protonmail.com>
2021-08-20 14:59:42 +0200

    example added

---
+++ examples/MPU6050_Basic/MPU6050_Basic.ino
@@ -0,0 +1,13 @@
+#include <MPU6050.h>
+
+MPU6050 pin(13);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pin.blink(true);
+}
